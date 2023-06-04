0c0fc68aa00ffcef6da0556c2c30b4c51ac4cd54
Shu DaCheng <pinkshudacheng@gmail.com>
2023-06-05 03:44:50 +0800

    ==

--- ios/Classes/pdfium_libs.c
+++ ios/Classes/pdfium_libs.c
@@ -1,10 +1,3 @@
 #include"fpdfview.h"
 
-#if _WIN32
-#define FFI_PLUGIN_EXPORT __declspec(dllexport)
-#else
-#define FFI_PLUGIN_EXPORT
-#endif
-FFI_PLUGIN_EXPORT intptr_t NullToDo() { 
-    FPDF_InitLibrary();
-    return 0; }
+void NullToDo() {FPDF_InitLibrary();}
