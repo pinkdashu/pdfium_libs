a6dba8325506104dd86164cfdf548f1d68d810ad
Shu DaCheng <pinkshudacheng@gmail.com>
2023-06-10 17:44:55 +0800

    ==

--- lib/pdfium_libs.dart
+++ lib/pdfium_libs.dart
@@ -1,6 +1,7 @@
 library pdfium_bindings;
 
 export 'src/exceptions.dart';
+export 'src/extensions.dart';
 export "src/pdfium_libs_bindings_generated.dart";
 export 'package:pdfium_libs/pdfium_libs.dart';
 export 'src/pdfium_libs.dart';
